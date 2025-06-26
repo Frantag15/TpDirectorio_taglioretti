a1dec4c6bc1d82d903ee27d57c53dc701e54cb0b
Frantag15 <48178331@ort>
2025-06-26 12:14:33 -0300

    cambios  en el estilo

--- TpDirectorio_taglioretti/src/pages/Home.jsx
+++ TpDirectorio_taglioretti/src/pages/Home.jsx
@@ -1,10 +1,23 @@
 import personas from "../data/personas.json";
 import PersonaCard from "../components/PersonaCard";
 import "./Home.css";
+import fondo from "../assets/imagenes/imagenfondo.jpg";
 
 export default function Home() {
+  const fondoEstilo = {
+    backgroundImage: `url(${fondo})`,
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    minHeight: "100vh",
+    width: "100%",
+    paddingTop: "6rem",
+
+    paddingBottom: "2rem",
+  };  
+
   return (
-    <div className="home-container">
+    <div className="home-container" style={fondoEstilo}>
+      <div className="overlay"></div> {/* Fondo negro transparente */}
       <h2 className="home-title">Listado de Personas</h2>
       <div className="cards-container">
         {personas.map((p) => (
